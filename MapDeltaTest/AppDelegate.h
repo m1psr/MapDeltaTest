84862a0aa7ddd0c20ac6a7607ff69ecf67336902
Mikhai1 <m@MacBook-Air-M.local>
2014-10-13 11:26:44 +0400

    Initial Commit

---
+++ MapDeltaTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapDeltaTest
+//
+//  Created by M on 13.10.14.
+//  Copyright (c) 2014 M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
